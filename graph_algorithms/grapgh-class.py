783ee3ef7b9c6c5c1e6a80a75b06d3f919328674
Dmitrii Skr <35507939+zhivou@users.noreply.github.com>
2022-05-21 23:47:08 -0400

    Add draft for is reachable method

--- graph_algorithms/grapgh-class.py
+++ graph_algorithms/grapgh-class.py
@@ -1,19 +1,48 @@
-class Grapgh:
-    def __init__(self, nodes: Node):
-        self.nodes = nodes
+from collections import defaultdict
 
+class Grapgh:
+    def __init__(self, V, nodes = defaultdict(list)):
+        """
+        We need V here only to know the size of the graph, 
+        we can't just understand it by looking at the Adjacency 
+        list as it can have loops.
+        """
+        self.V = V
+        self.nodes = nodes
+        
+    def add_edge(self, u, v):
+        self.nodes[u].append(v)
+        
+    def is_reachable(self, u, v):
+        """
+        We going to use visited array as mapping for visited nodes,
+        this will keep track of possible loops.
+        index will be eq to node.value
+        value will be boolean representing visted or not nodes
+        """
+        visited = [False] * self.V
+        queue = []
+        
+        visited[u] = True
+        queue.append(u)
+        
+        while queue:
+            n = queue.pop(0)
+            print(n)
+        
+        
+        
+g = Grapgh(4)
+g.add_edge(0,1)
+g.add_edge(1,2)
+g.add_edge(2,3)
+g.add_edge(3,3)
+
+g.is_reachable(1,3)
+
+# Can also be represented as a Node class.
 class Node:
     def __init__(self, name, children: Node) :
         self.name = name
         self.children = children
 
-nodes = [
-    Node(6, [Node(5, None)]),
-    Node(5, [Node(4, None)]),
-    Node(4, [Node(6, None)]),
-    Node(0, [Node(1, None)]),
-    Node(1, [Node(2, None)]),
-    Node(2, [Node(0, None), Node(3, None)]),
-    Node(3, [Node(2, None)])
-]
-g = Grapgh(nodes)
