6033383785b2ca89fb2dd683e3f38b52a011d17c
Dmitrii Skr <35507939+zhivou@users.noreply.github.com>
2022-05-22 15:19:14 -0400

    Complete alghorithm

--- graph_algorithms/grapgh-class.py
+++ graph_algorithms/grapgh-class.py
@@ -9,10 +9,10 @@ class Grapgh:
         """
         self.V = V
         self.nodes = nodes
-        
+
     def add_edge(self, u, v):
         self.nodes[u].append(v)
-        
+
     def is_reachable(self, u, v):
         """
         We going to use visited array as mapping for visited nodes,
@@ -20,25 +20,41 @@ class Grapgh:
         index will be eq to node.value
         value will be boolean representing visted or not nodes
         """
+        if(u == v):
+            return True
+            
         visited = [False] * self.V
         queue = []
+        route = []
         
         visited[u] = True
         queue.append(u)
         
         while queue:
             n = queue.pop(0)
-            print(n)
+
+            # If visiting node is eq to v it means it was
+            # reached and can return True as reachable node.
+            if(n == v):
+                return True
+            
+            for i in self.nodes[n]:
+                if (visited[i] == False):
+                    visited[i] = True
+                    queue.append(i)
         
+        return False
         
         
 g = Grapgh(4)
-g.add_edge(0,1)
-g.add_edge(1,2)
-g.add_edge(2,3)
-g.add_edge(3,3)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
 
-g.is_reachable(1,3)
+g.is_reachable(2,3)
 
 # Can also be represented as a Node class.
 class Node:
